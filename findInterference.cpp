3eba1625942af68a90e560f44e3e40c870de7921
govoni <pietro.govoni@gmail.com>
2013-07-17 14:45:08 +0200

    add reweighting

--- findInterference.cpp
+++ findInterference.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <iterator>
 #include <cstdlib>
+#include <cassert>
 
 #include "TH1.h"
 #include "TFile.h"
@@ -15,6 +16,9 @@
 // CINT does not understand some files included by LorentzVector
 #include "Math/Vector3D.h"
 #include "Math/Vector4D.h"
+
+#include "LHAPDF/LHAPDF.h"
+
 
 using namespace ROOT::Math ;
 using namespace std ;
@@ -30,6 +34,32 @@ struct ptsort: public std::binary_function<TLorentzVector, TLorentzVector, bool>
       return  (x.Perp () < y.Perp () ) ;
     }
 } ;
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+
+
+struct etasort: public std::binary_function<TLorentzVector, TLorentzVector, bool>
+{
+  bool operator () (const TLorentzVector & x, const TLorentzVector & y)
+    {
+      return  (x.Eta () < y.Eta () ) ;
+    }
+} ;
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+pair<int, int> findPairWithLargestDeta (const vector<TLorentzVector> & v_f_quarks)
+{
+  int minimum = min_element (v_f_quarks.begin (), v_f_quarks.end (), etasort ()) - v_f_quarks.begin () ;
+  int maximum = max_element (v_f_quarks.begin (), v_f_quarks.end (), etasort ()) - v_f_quarks.begin () ;
+  return pair<int, int> (minimum, maximum) ;
+  
+}
 
 
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -92,9 +122,55 @@ lorentzVector buildLP (const LHEF::HEPEUP & event, int iPart)
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 
-int fillWWhist (LHEF::Reader & reader, TH1F * h_MWW, double XS)
+struct histos
 {
-  int totalCount = 0 ;
+  TH1F *  m_h_MWW ;
+  TH1F *  m_h_scale ;
+  TString m_name ;
+  double  m_XS ;
+  
+  histos (TString name, double XS) : m_name (name), m_XS (XS)
+    {
+      m_h_MWW = new TH1F (TString ("h_MWW_") + name, 
+                          TString ("h_MWW_") + name, 50, 200., 2000.) ;
+      m_h_MWW->Sumw2 () ;
+      m_h_scale = new TH1F (TString ("h_scale_") + name, 
+                          TString ("h_scale_") + name, 100, 0., 1000.) ;
+      m_h_scale->Sumw2 () ;
+    }
+ 
+  void norm (double total = 0)
+    {
+      double factor = m_XS / m_h_MWW->GetEntries () ;
+      if (total != 0) factor = m_XS / total ;
+      m_h_MWW->Scale (factor) ;
+      m_h_scale->Scale (factor) ;
+    }
+  
+  ~histos ()
+    {
+      delete m_h_MWW ;
+      delete m_h_scale ;
+    }  
+    
+  void save (TFile & outfile) 
+    {
+      outfile.cd () ;
+      m_h_MWW->Write () ;
+      m_h_scale->Write () ;
+    }
+  
+
+} ;
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+double 
+fillHistos (LHEF::Reader & reader, histos & Histos, double XS, double referenceScale = 0)
+{
+  double totalCount = 0. ;
    
   //PG loop over input events
   while (reader.readEvent ()) 
@@ -106,10 +182,22 @@ int fillWWhist (LHEF::Reader & reader, TH1F * h_MWW, double XS)
       vector<TLorentzVector> v_f_leptons ;
       vector<TLorentzVector> v_f_neutrinos ;
       
+      double x[2] = {0., 0.} ;
+      int flavour[2] = {0, 0} ;
+
       // loop over particles in the event
       for (int iPart = 0 ; iPart < reader.hepeup.IDUP.size (); ++iPart) 
         {
           TLorentzVector dummy = buildP (reader.hepeup, iPart) ;
+
+           // incoming particles        
+           if (reader.hepeup.ISTUP.at (iPart) == -1) 
+             {
+               x[iPart] = dummy.P () / 4000. ;
+               flavour[iPart] = reader.hepeup.IDUP.at (iPart) ;
+
+             } // incoming particle          
+
           // intermediate particles          
           if (reader.hepeup.ISTUP.at (iPart) == 2)
             {
@@ -141,15 +229,36 @@ int fillWWhist (LHEF::Reader & reader, TH1F * h_MWW, double XS)
          } // outgoing particles
         } // loop over particles in the event
 
+      pair<int, int> detaIndices = findPairWithLargestDeta (v_f_quarks) ;
+      if (v_f_quarks.at (detaIndices.second).Eta () - v_f_quarks.at (detaIndices.first).Eta () < 2) continue ;
+      TLorentzVector largestPair = v_f_quarks.at (detaIndices.second) + v_f_quarks.at (detaIndices.first) ;
+      if (largestPair.M () < 100) continue ; //PG selection applied in phantom
+
+      int cont = 0 ;
+      for (int iJ = 0 ; iJ < 4 ; ++iJ)
+        for (int iJ2 = iJ + 1 ; iJ2 < 4 ; ++iJ2)
+          if (v_f_quarks.at (iJ).DeltaR (v_f_quarks.at (iJ2)) < 0.4) cont = 1 ;
+      if (cont == 1) continue ;
+
       //PG the first two are the VBF jets, the following ones the W jets
       sort (v_f_quarks.rbegin (), v_f_quarks.rend (), ptsort ()) ;  
       TLorentzVector total = (v_f_leptons.at (0) + v_f_neutrinos.at (0)) + (v_f_quarks.at (2) + v_f_quarks.at (3)) ;
 
-      h_MWW->Fill (total.M ()) ;
-      ++totalCount ;
+      //PG the scale:
+      float scale = reader.hepeup.SCALUP ;
+      Histos.m_h_scale->Fill (scale) ;
+
+      double weight = 1. ;
+      if (referenceScale != 0 )
+        weight = LHAPDF::xfx (x[0], referenceScale, flavour[0]) * LHAPDF::xfx (x[1], referenceScale, flavour[1]) /
+                 (LHAPDF::xfx (x[0], scale, flavour[0]) * LHAPDF::xfx (x[1], scale, flavour[1])) ;
+
+      Histos.m_h_MWW->Fill (total.M ()) ;
+      totalCount += weight ;
+
     } //PG loop over input events
 
-  h_MWW->Scale (XS / totalCount) ;
+  Histos.norm (totalCount) ;
 
   return totalCount ;
   
@@ -158,11 +267,17 @@ int fillWWhist (LHEF::Reader & reader, TH1F * h_MWW, double XS)
 // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 
-int main(int argc, char ** argv) 
+int main (int argc, char ** argv) 
 {
-  int nbins = 50 ;
-  float hmin = 200. ; 
-  float hmax = 2000. ;
+
+  const int SUBSET = 0 ;
+  const string NAME = "cteq6ll" ; //"cteq6l1"
+
+  LHAPDF::initPDFSet (NAME, LHAPDF::LHPDF, SUBSET) ;
+  const int NUMBER = LHAPDF::numberPDF () ;
+
+  LHAPDF::initPDF (0) ;
+
 
   //PG ---- madgraph ---- signal only
   
@@ -171,8 +286,8 @@ int main(int argc, char ** argv)
   
   std::ifstream ifs_mg (filename_mg.c_str ()) ;
   LHEF::Reader reader_mg (ifs_mg) ;
-  TH1F * h_MWW_mg = new TH1F ("h_MWW_mg", "h_MWW_mg", nbins, hmin, hmax) ;
-  int entries_mg = fillWWhist (reader_mg, h_MWW_mg, XS_mg) ;
+  histos H_mg ("mg", XS_mg) ;
+  double entries_mg = fillHistos (reader_mg, H_mg, XS_mg, 500.) ;
 
   cout << "madgraph events : " << entries_mg << endl ;
   
@@ -183,8 +298,8 @@ int main(int argc, char ** argv)
   
   std::ifstream ifs_phbkg (filename_phbkg.c_str ()) ;
   LHEF::Reader reader_phbkg (ifs_phbkg) ;
-  TH1F * h_MWW_phbkg = new TH1F ("h_MWW_phbkg", "h_MWW_phbkg", nbins, hmin, hmax) ;
-  int entries_phbkg = fillWWhist (reader_phbkg, h_MWW_phbkg, XS_phbkg) ;
+  histos H_phbkg ("phbkg", XS_phbkg) ;
+  double entries_phbkg = fillHistos (reader_phbkg, H_phbkg, XS_phbkg, 500.) ;
 
   cout << "phantom bkg events : " << entries_phbkg << endl ;
 
@@ -196,21 +311,17 @@ int main(int argc, char ** argv)
   
   std::ifstream ifs_phbkgsig (filename_phbkgsig.c_str ()) ;
   LHEF::Reader reader_phbkgsig (ifs_phbkgsig) ;
-  TH1F * h_MWW_phbkgsig = new TH1F ("h_MWW_phbkgsig", "h_MWW_phbkgsig", nbins, hmin, hmax) ;
-  int entries_phbkgsig = fillWWhist (reader_phbkgsig, h_MWW_phbkgsig, XS_phbkgsig) ;
+  histos H_phbkgsig ("phbkgsig", XS_phbkgsig) ;
+  double entries_phbkgsig = fillHistos (reader_phbkgsig, H_phbkgsig, XS_phbkgsig, 500.) ;
 
   cout << "phantom bkg+sig events : " << entries_phbkgsig << endl ;
 
-  //PG operations
-  
-  TH1F * h_subtr = (TH1F *) h_MWW_phbkgsig->Clone ("h_subtr") ;
-  h_subtr->Add (h_MWW_mg, -1) ;
+  //PG saving the histograms
 
   TFile f ("findInterference.root", "recreate") ;
-  h_MWW_mg->Write () ;
-  h_MWW_phbkg->Write () ;
-  h_MWW_phbkgsig->Write () ;
-  h_subtr->Write () ;
+  H_phbkgsig.save (f) ;
+  H_phbkg.save (f) ;
+  H_mg.save (f) ;
   f.Close () ;
 
   return 0 ;
@@ -225,10 +336,10 @@ h_MWW_phbkgsig->SetStats (0)
 h_MWW_mg->SetStats (0)
 h_MWW_phbkg->SetLineColor (kOrange)
 h_MWW_phbkg->SetLineWidth (2)
-h_MWW_phbkg->Draw ("same")
+h_MWW_phbkg->Draw ("histsame")
 h_MWW_phbkgsig->SetLineColor (kRed)
 h_MWW_phbkgsig->SetLineWidth (2)
-h_MWW_phbkgsig->Draw ("same")
+h_MWW_phbkgsig->Draw ("histsame")
 
 TH1F * diff = (TH1F *) h_MWW_phbkgsig->Clone ("diff")
 diff->SetTitle ("")
@@ -240,17 +351,17 @@ ratio->Divide (diff)
 cout << "scaling by " << 1. / ratio->GetBinContent (ratio->FindBin (500)) << endl ;
 //h_MWW_mg->Scale (1. / ratio->GetBinContent (ratio->FindBin (500)))
 
-h_MWW_mg->Draw ("same")
+h_MWW_mg->Draw ("histsame")
 
 TCanvas c2
-diff->Draw ()
+diff->Draw ("hist")
 
 TCanvas c3
-ratio->Draw ()
+ratio->Draw ("hist")
 
 TCanvas c4
-diff->Draw ()
-h_MWW_mg->Draw ("same")
+diff->Draw ("hist")
+h_MWW_mg->Draw ("histsame")
 
 
 */
