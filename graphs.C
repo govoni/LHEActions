11aeab4ea13f5cf31ee46751fffdf8af444890d6
govoni <pietro.govoni@gmail.com>
2013-09-04 21:10:29 +0200

    first commit

---
+++ graphs.C
@@ -0,0 +1,405 @@
+
+
+/*** double crystall ball ***/
+double crystalBallLowHigh (double* x, double* par)
+{
+  //[0] = N
+  //[1] = mean
+  //[2] = sigma
+  //[3] = alpha
+  //[4] = n
+  //[5] = alpha2
+  //[6] = n2
+  
+  double xx = x[0];
+  double mean = par[1];
+  double sigma = par[2];
+  double alpha = par[3];
+  double n = par[4];
+  double alpha2 = par[5];
+  double n2 = par[6];
+
+  if( (xx-mean)/sigma > fabs(alpha) )
+  {
+    double A = pow(n/fabs(alpha), n) * exp(-0.5 * alpha*alpha);
+    double B = n/fabs(alpha) - fabs(alpha);
+    
+    return par[0] * A * pow(B + (xx-mean)/sigma, -1.*n);
+  }
+  
+  else if( (xx-mean)/sigma < -1.*fabs(alpha2) )
+  {
+    double A = pow(n2/fabs(alpha2), n2) * exp(-0.5 * alpha2*alpha2);
+    double B = n2/fabs(alpha2) - fabs(alpha2);
+    
+    return par[0] * A * pow(B - (xx-mean)/sigma, -1.*n2);
+  }
+  
+  else
+  {
+    return par[0] * exp(-1. * (xx-mean)*(xx-mean) / (2*sigma*sigma) );
+  }
+  
+}
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+Double_t doublePeakModel (Double_t * xx, Double_t * par)
+{
+  double scale    = par[0] ;
+  double shift    = par[1] ;
+  double distance = par[2] ;
+  double gamma    = par[3] ;
+  double x = xx[0] - shift ;
+
+  double norm = 1. / (shift * shift + gamma) - 1 / ((shift + 2 * distance) * (shift + 2 * distance) + gamma) ;
+  return scale * (1. / norm) * ( 1. / ((x - distance) * (x - distance) + gamma) - 1 / ((x + distance) * (x + distance) + gamma)) ;
+  return scale * ( 1. / ((x - distance) * (x - distance) + gamma) - 1 / ((x + distance) * (x + distance) + gamma)) ;
+}
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+Double_t intereferenceOverSignal (Double_t * xx, Double_t * par) 
+{
+  if (crystalBallLowHigh (xx, par + 4) < 0.0000001) return 0 ;
+  return doublePeakModel (xx, par) / crystalBallLowHigh (xx, par + 4) ;
+}
+
+
+// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+
+int graphs () 
+{
+  TGraph * tg_par0 = new TGraph (5) ;
+  TGraph * tg_par1 = new TGraph (5) ;
+  TGraph * tg_par2 = new TGraph (5) ;
+  TGraph * tg_par3 = new TGraph (5) ;
+  
+  TGraph * tg_sig_par0 = new TGraph (5) ;
+  TGraph * tg_sig_par1 = new TGraph (5) ;
+  TGraph * tg_sig_par2 = new TGraph (5) ;
+  TGraph * tg_sig_par3 = new TGraph (5) ;
+  TGraph * tg_sig_par4 = new TGraph (5) ;
+  TGraph * tg_sig_par5 = new TGraph (5) ;
+  TGraph * tg_sig_par6 = new TGraph (5) ;
+
+  int i = 0 ; 
+  
+
+  
+  // ----> MASS 350 ---- ---- ---- 
+
+  // interference parametrisation:
+  tg_par0->SetPoint (i, 350, -4.43615e-07) ;
+  tg_par1->SetPoint (i, 350, 349.521) ;
+  tg_par2->SetPoint (i, 350, 0.000804281) ;
+  tg_par3->SetPoint (i, 350, 660.304) ;
+  TF1 * func_350 = new TF1 ("func_350",doublePeakModel, 200, 2000, 4) ;
+  double params_350[4] = {-4.43615e-07, 349.521, 0.000804281, 660.304 } ;
+  func_350->SetParameters (params_350) ;
+  // signal only parametrisation:
+  tg_sig_par0->SetPoint (i, 350, 0.00690448) ;
+  tg_sig_par1->SetPoint (i, 350, 350.413) ;
+  tg_sig_par2->SetPoint (i, 350, -9.7067) ;
+  tg_sig_par3->SetPoint (i, 350, -5.11895e+06) ;
+  tg_sig_par4->SetPoint (i, 350, -44031.8) ;
+  tg_sig_par5->SetPoint (i, 350, 1.35628) ;
+  tg_sig_par6->SetPoint (i, 350, 1.41093) ;
+  TF1 * func_sig_350 = new TF1 ("func_sig_350",crystalBallLowHigh, 200, 2000, 7) ;
+  double params_sig_350[7] = {0.00690448, 350.413, -9.7067, -5.11895e+06, -44031.8, 1.35628, 1.41093 } ;
+  func_sig_350->SetParameters (params_sig_350) ;
+  i++ ;
+  
+  // ----> MASS 500 ---- ---- ---- 
+
+  // interference parametrisation:
+  tg_par0->SetPoint (i, 500, -4.30534e-06) ;
+  tg_par1->SetPoint (i, 500, 488.466) ;
+  tg_par2->SetPoint (i, 500, -0.00047267) ;
+  tg_par3->SetPoint (i, 500, 9775.12) ;
+  TF1 * func_500 = new TF1 ("func_500",doublePeakModel, 200, 2000, 4) ;
+  double params_500[4] = {-4.30534e-06, 488.466, -0.00047267, 9775.12 } ;
+  func_500->SetParameters (params_500) ;
+  // signal only parametrisation:
+  tg_sig_par0->SetPoint (i, 500, 0.00145953) ;
+  tg_sig_par1->SetPoint (i, 500, 504.022) ;
+  tg_sig_par2->SetPoint (i, 500, -28.0402) ;
+  tg_sig_par3->SetPoint (i, 500, 1.00869) ;
+  tg_sig_par4->SetPoint (i, 500, 4.76843) ;
+  tg_sig_par5->SetPoint (i, 500, 0.815444) ;
+  tg_sig_par6->SetPoint (i, 500, 2.43023) ;
+  TF1 * func_sig_500 = new TF1 ("func_sig_500",crystalBallLowHigh, 200, 2000, 7) ;
+  double params_sig_500[7] = {0.00145953, 504.022, -28.0402, 1.00869, 4.76843, 0.815444, 2.43023 } ;
+  func_sig_500->SetParameters (params_sig_500) ;
+  i++ ;
+  
+  // ----> MASS 650 ---- ---- ---- 
+
+  // interference parametrisation:
+  tg_par0->SetPoint (i, 650, -2.15084e-06) ;
+  tg_par1->SetPoint (i, 650, 648.511) ;
+  tg_par2->SetPoint (i, 650, -0.00519707) ;
+  tg_par3->SetPoint (i, 650, 23441.7) ;
+  TF1 * func_650 = new TF1 ("func_650",doublePeakModel, 200, 2000, 4) ;
+  double params_650[4] = {-2.15084e-06, 648.511, -0.00519707, 23441.7 } ;
+  func_650->SetParameters (params_650) ;
+  // signal only parametrisation:
+  tg_sig_par0->SetPoint (i, 650, 0.000287084) ;
+  tg_sig_par1->SetPoint (i, 650, 662.242) ;
+  tg_sig_par2->SetPoint (i, 650, -73.0336) ;
+  tg_sig_par3->SetPoint (i, 650, 1.60754) ;
+  tg_sig_par4->SetPoint (i, 650, 1.70268) ;
+  tg_sig_par5->SetPoint (i, 650, 1.03035) ;
+  tg_sig_par6->SetPoint (i, 650, 4.15882) ;
+  TF1 * func_sig_650 = new TF1 ("func_sig_650",crystalBallLowHigh, 200, 2000, 7) ;
+  double params_sig_650[7] = {0.000287084, 662.242, -73.0336, 1.60754, 1.70268, 1.03035, 4.15882 } ;
+  func_sig_650->SetParameters (params_sig_650) ;
+  i++ ;
+  
+  // ----> MASS 800 ---- ---- ---- 
+
+  // interference parametrisation:
+  tg_par0->SetPoint (i, 800, -1.23195e-06) ;
+  tg_par1->SetPoint (i, 800, 763.259) ;
+  tg_par2->SetPoint (i, 800, -14.7866) ;
+  tg_par3->SetPoint (i, 800, 35942.1) ;
+  TF1 * func_800 = new TF1 ("func_800",doublePeakModel, 200, 2000, 4) ;
+  double params_800[4] = {-1.23195e-06, 763.259, -14.7866, 35942.1 } ;
+  func_800->SetParameters (params_800) ;
+  // signal only parametrisation:
+  tg_sig_par0->SetPoint (i, 800, 8.50641e-05) ;
+  tg_sig_par1->SetPoint (i, 800, 808.144) ;
+  tg_sig_par2->SetPoint (i, 800, -118.646) ;
+  tg_sig_par3->SetPoint (i, 800, 1.3644) ;
+  tg_sig_par4->SetPoint (i, 800, 1.47356) ;
+  tg_sig_par5->SetPoint (i, 800, 1.40517) ;
+  tg_sig_par6->SetPoint (i, 800, 3.18969) ;
+  TF1 * func_sig_800 = new TF1 ("func_sig_800",crystalBallLowHigh, 200, 2000, 7) ;
+  double params_sig_800[7] = {8.50641e-05, 808.144, -118.646, 1.3644, 1.47356, 1.40517, 3.18969 } ;
+  func_sig_800->SetParameters (params_sig_800) ;
+  i++ ;
+  
+  // ----> MASS 1000 ---- ---- ---- 
+
+  // interference parametrisation:
+  tg_par0->SetPoint (i, 1000, -1.98084e-06) ;
+  tg_par1->SetPoint (i, 1000, 797.034) ;
+  tg_par2->SetPoint (i, 1000, 211.975) ;
+  tg_par3->SetPoint (i, 1000, 50380.7) ;
+  TF1 * func_1000 = new TF1 ("func_1000",doublePeakModel, 200, 2000, 4) ;
+  double params_1000[4] = {-1.98084e-06, 797.034, 211.975, 50380.7 } ;
+  func_1000->SetParameters (params_1000) ;
+  // signal only parametrisation:
+  tg_sig_par0->SetPoint (i, 1000, 6.56045e-05) ;
+  tg_sig_par1->SetPoint (i, 1000, 937.602) ;
+  tg_sig_par2->SetPoint (i, 1000, -216.078) ;
+  tg_sig_par3->SetPoint (i, 1000, 1.42632) ;
+  tg_sig_par4->SetPoint (i, 1000, 1.55226) ;
+  tg_sig_par5->SetPoint (i, 1000, 1.55132) ;
+  tg_sig_par6->SetPoint (i, 1000, 2.79578) ;
+  TF1 * func_sig_1000 = new TF1 ("func_sig_1000",crystalBallLowHigh, 200, 2000, 7) ;
+  double params_sig_1000[7] = {6.56045e-05, 937.602, -216.078, 1.42632, 1.55226, 1.55132, 2.79578 } ;
+  func_sig_1000->SetParameters (params_sig_1000) ;
+  i++ ;
+
+
+
+
+  //PG plotting
+  //PG ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+  
+  tg_par0->SetTitle ("") ;
+  tg_par1->SetTitle ("") ;
+  tg_par2->SetTitle ("") ;
+  tg_par3->SetTitle ("") ;
+  
+  tg_par0->GetYaxis ()->SetTitle ("scale") ;
+  tg_par1->GetYaxis ()->SetTitle ("shift") ;
+  tg_par2->GetYaxis ()->SetTitle ("distance") ;
+  tg_par3->GetYaxis ()->SetTitle ("gamma") ;
+  
+  tg_par0->GetXaxis ()->SetTitle ("m_{WW}") ;
+  tg_par1->GetXaxis ()->SetTitle ("m_{WW}") ;
+  tg_par2->GetXaxis ()->SetTitle ("m_{WW}") ;
+  tg_par3->GetXaxis ()->SetTitle ("m_{WW}") ;
+
+  TCanvas * c_par = new TCanvas () ;
+  c_par->Divide (2,2) ;
+  i = 0 ;
+  c_par->cd (++i) ; tg_par0->Draw ("AL*") ; 
+  c_par->cd (++i) ; tg_par1->Draw ("AL*") ; // tg_mass->Draw ("L") ;
+  c_par->cd (++i) ; tg_par2->Draw ("AL*") ; // tg_width->Draw ("L") ;
+  c_par->cd (++i) ; tg_par3->Draw ("AL*") ; // tg_width->Draw ("L") ;
+  
+  TCanvas * c_sig_par = new TCanvas () ;
+  c_sig_par->Divide (3,2) ;
+  i = 0 ;
+  c_sig_par->cd (++i) ; tg_sig_par0->Draw ("AL*") ; 
+  c_sig_par->cd (++i) ; tg_sig_par1->Draw ("AL*") ;
+  c_sig_par->cd (++i) ; tg_sig_par2->Draw ("AL*") ;
+  c_sig_par->cd (++i) ; tg_sig_par3->Draw ("AL*") ;
+  c_sig_par->cd (++i) ; tg_sig_par4->Draw ("AL*") ;
+  c_sig_par->cd (++i) ; tg_sig_par5->Draw ("AL*") ;
+  c_sig_par->cd (++i) ; tg_sig_par6->Draw ("AL*") ;
+
+  TCanvas * c_results = new TCanvas ("c_results", "c_results", 5000, 600) ;
+  c_results->Divide (5,2) ;
+
+  i = 1 ;
+  double mass = 350 ;
+  double rangeScale = 1.5 ;
+
+  // 350 gev ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+  mass = 350 ;
+  rangeScale = 1.5 ;
+  if (mass[i] > 480) rangeScale = 2 ;
+
+  Double_t IOS_350_pars[11] ;
+  for (int j = 0 ; j < 4 ; ++j) IOS_350_pars[j] = params_350[j] ;
+  for (int j = 0 ; j < 7 ; ++j) IOS_350_pars[j + 4] = params_sig_350[j] ;
+  TF1 * f_IOS_350 = new TF1 ("f_IOS_350", intereferenceOverSignal, 200, 2000, 11) ;
+  f_IOS_350->SetParameters (IOS_350_pars) ;
+
+  c_results->cd (i) ;
+  gPad->DrawFrame (200, 1.1 * func_350->GetMinimum (), rangeScale * mass, 1.1 * func_sig_350->GetMaximum ()) ;
+  func_sig_350->SetLineWidth (1) ;
+  func_sig_350->SetNpx (10000) ;
+  func_sig_350->Draw ("same") ;
+  func_350->SetLineWidth (1) ;
+  func_350->SetNpx (10000) ;
+  func_350->Draw ("same") ;
+  c_results->cd (5 + i++) ;
+  gPad->DrawFrame (200, 1.1 * f_IOS_350->GetMinimum (), rangeScale * mass, 1.1 * f_IOS_350->GetMaximum ()) ;
+  f_IOS_350->SetLineWidth (1) ;
+  f_IOS_350->SetNpx (10000) ;
+  f_IOS_350->Draw ("same") ;
+ 
+
+
+  // 500 gev ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+  mass = 500 ;
+  rangeScale = 1.5 ;
+  if (mass[i] > 480) rangeScale = 2 ;
+
+  Double_t IOS_500_pars[11] ;
+  for (int j = 0 ; j < 4 ; ++j) IOS_500_pars[j] = params_500[j] ;
+  for (int j = 0 ; j < 7 ; ++j) IOS_500_pars[j + 4] = params_sig_500[j] ;
+  TF1 * f_IOS_500 = new TF1 ("f_IOS_500", intereferenceOverSignal, 200, 2000, 11) ;
+  f_IOS_500->SetParameters (IOS_500_pars) ;
+
+  c_results->cd (i) ;
+  gPad->DrawFrame (200, 1.1 * func_500->GetMinimum (), rangeScale * mass, 1.1 * func_sig_500->GetMaximum ()) ;
+  func_sig_500->SetLineWidth (1) ;
+  func_sig_500->SetNpx (10000) ;
+  func_sig_500->Draw ("same") ;
+  func_500->SetLineWidth (1) ;
+  func_500->SetNpx (10000) ;
+  func_500->Draw ("same") ;
+  c_results->cd (5 + i++) ;
+  gPad->DrawFrame (200, 1.1 * f_IOS_500->GetMinimum (), rangeScale * mass, 1.1 * f_IOS_500->GetMaximum ()) ;
+  f_IOS_500->SetLineWidth (1) ;
+  f_IOS_500->SetNpx (10000) ;
+  f_IOS_500->Draw ("same") ;
+ 
+
+
+  // 650 gev ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+  mass = 650 ;
+  rangeScale = 1.5 ;
+  if (mass[i] > 480) rangeScale = 2 ;
+
+  Double_t IOS_650_pars[11] ;
+  for (int j = 0 ; j < 4 ; ++j) IOS_650_pars[j] = params_650[j] ;
+  for (int j = 0 ; j < 7 ; ++j) IOS_650_pars[j + 4] = params_sig_650[j] ;
+  TF1 * f_IOS_650 = new TF1 ("f_IOS_650", intereferenceOverSignal, 200, 2000, 11) ;
+  f_IOS_650->SetParameters (IOS_650_pars) ;
+
+  c_results->cd (i) ;
+  gPad->DrawFrame (200, 1.1 * func_650->GetMinimum (), rangeScale * mass, 1.1 * func_sig_650->GetMaximum ()) ;
+  func_sig_650->SetLineWidth (1) ;
+  func_sig_650->SetNpx (10000) ;
+  func_sig_650->Draw ("same") ;
+  func_650->SetLineWidth (1) ;
+  func_650->SetNpx (10000) ;
+  func_650->Draw ("same") ;
+  c_results->cd (5 + i++) ;
+  gPad->DrawFrame (200, 1.1 * f_IOS_650->GetMinimum (), rangeScale * mass, 1.1 * f_IOS_650->GetMaximum ()) ;
+  f_IOS_650->SetLineWidth (1) ;
+  f_IOS_650->SetNpx (10000) ;
+  f_IOS_650->Draw ("same") ;
+ 
+
+
+  // 800 gev ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+  mass = 800 ;
+  rangeScale = 1.5 ;
+  if (mass[i] > 480) rangeScale = 2 ;
+
+  Double_t IOS_800_pars[11] ;
+  for (int j = 0 ; j < 4 ; ++j) IOS_800_pars[j] = params_800[j] ;
+  for (int j = 0 ; j < 7 ; ++j) IOS_800_pars[j + 4] = params_sig_800[j] ;
+  TF1 * f_IOS_800 = new TF1 ("f_IOS_800", intereferenceOverSignal, 200, 2000, 11) ;
+  f_IOS_800->SetParameters (IOS_800_pars) ;
+
+  c_results->cd (i) ;
+  gPad->DrawFrame (200, 1.1 * func_800->GetMinimum (), rangeScale * mass, 1.1 * func_sig_800->GetMaximum ()) ;
+  func_sig_800->SetLineWidth (1) ;
+  func_sig_800->SetNpx (10000) ;
+  func_sig_800->Draw ("same") ;
+  func_800->SetLineWidth (1) ;
+  func_800->SetNpx (10000) ;
+  func_800->Draw ("same") ;
+  c_results->cd (5 + i++) ;
+  gPad->DrawFrame (200, 1.1 * f_IOS_800->GetMinimum (), rangeScale * mass, 1.1 * f_IOS_800->GetMaximum ()) ;
+  f_IOS_800->SetLineWidth (1) ;
+  f_IOS_800->SetNpx (10000) ;
+  f_IOS_800->Draw ("same") ;
+ 
+
+
+  // 350 gev ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- 
+
+  mass = 1000 ;
+  rangeScale = 1.5 ;
+  if (mass[i] > 480) rangeScale = 2 ;
+
+  Double_t IOS_1000_pars[11] ;
+  for (int j = 0 ; j < 4 ; ++j) IOS_1000_pars[j] = params_1000[j] ;
+  for (int j = 0 ; j < 7 ; ++j) IOS_1000_pars[j + 4] = params_sig_1000[j] ;
+  TF1 * f_IOS_1000 = new TF1 ("f_IOS_1000", intereferenceOverSignal, 200, 2000, 11) ;
+  f_IOS_1000->SetParameters (IOS_1000_pars) ;
+
+  c_results->cd (i) ;
+  gPad->DrawFrame (200, 1.1 * func_1000->GetMinimum (), rangeScale * mass, 1.1 * func_sig_1000->GetMaximum ()) ;
+  func_sig_1000->SetLineWidth (1) ;
+  func_sig_1000->SetNpx (10000) ;
+  func_sig_1000->Draw ("same") ;
+  func_1000->SetLineWidth (1) ;
+  func_1000->SetNpx (10000) ;
+  func_1000->Draw ("same") ;
+  c_results->cd (5 + i++) ;
+  gPad->DrawFrame (200, 1.1 * f_IOS_1000->GetMinimum (), rangeScale * mass, 1.1 * f_IOS_1000->GetMaximum ()) ;
+  f_IOS_1000->SetLineWidth (1) ;
+  f_IOS_1000->SetNpx (10000) ;
+  f_IOS_1000->Draw ("same") ;
+ 
+
+
+
+  TCanvas * test = new TCanvas () ;
+  f_IOS_350->Draw () ;
+//  func_sig_350->Draw () ;
+//  func_350->Draw ("same") ;
+
+
+}  
+  
